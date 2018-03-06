325bad0f3d64c1ccdaa7d74c467fce81d803a6cd
Yousan_O <yousan@l2tp.org>
2018-03-06 14:45:32 +0900

    add js

--- wp-simple-pageview-count.php
+++ wp-simple-pageview-count.php
@@ -14,7 +14,38 @@
 
 class WSPC_AdminOptions {
 	public static function add_menu() {
-		add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function = '', $icon_url = '',
-			$position = null );
+		add_action( 'admin_menu', array( 'WSPC_AdminOptions', 'submenu' ) );
+	}
+
+	public static function submenu() {
+		add_submenu_page(
+			'options-general.php',
+			'My Custom Submenu Page',
+			'My Custom Submenu Page',
+			'manage_options',
+			'my-custom-submenu-page',
+			'wpdocs_my_custom_submenu_page_callback' );
+	}
+
+	function wpdocs_my_custom_submenu_page_callback() {
+		echo '<div class="wrap"><div id="icon-tools" class="icon32"></div>';
+		echo '<h2>My Custom Submenu Page</h2>';
+		echo '</div>';
 	}
 }
+
+WSPC_AdminOptions::add_menu();
+
+class WSPC_Loader {
+	public static function load() {
+		add_action('wp_enqueue_scripts', array('WSPC_Loader', 'enqueue_scripts'));
+	}
+
+	public static function enqueue_scripts() {
+//		var_dump(plugins_url('/js/wpspc.js', __FILE__));
+//		 exit;
+		wp_enqueue_script( 'wspc', plugins_url('/js/wspc.js', __FILE__), array('jquery') );
+	}
+}
+
+WSPC_Loader::load();
