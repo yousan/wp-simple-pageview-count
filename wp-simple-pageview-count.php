6464688a5586297afce32dce839bb72cf3dd9551
Yousan_O <yousan@l2tp.org>
2018-03-06 14:27:37 +0900

    Automatically commit

---
+++ wp-simple-pageview-count.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Wp Simple Pageview Count
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-simple-pageview-count
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Simple_Pageview_Count
+ */
