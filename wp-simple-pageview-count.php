b5f6a06080d69fc32564fdd36a296974fd13929a
Yousan_O <yousan@l2tp.org>
2018-03-06 14:33:16 +0900

    Automatically commit

--- wp-simple-pageview-count.php
+++ wp-simple-pageview-count.php
@@ -1,13 +1,20 @@
 <?php
 /**
  * Plugin Name:     Wp Simple Pageview Count
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:      https://github.com/yousan/wp-simple-pageview-count
+ * Description:     Count pageviews by Ajax request. Reduce requests with sampling rate.
+ * Author:          Yousan_O
+ * Author URI:      https://github.com/yousan
  * Text Domain:     wp-simple-pageview-count
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         0.0.1
  *
  * @package         Wp_Simple_Pageview_Count
  */
+
+class WSPC_AdminOptions {
+	public static function add_menu() {
+		add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function = '', $icon_url = '',
+			$position = null );
+	}
+}
